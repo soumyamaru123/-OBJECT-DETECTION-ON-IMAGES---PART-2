53ec2e5745e6ffb3c0cc57ea7c7493e247ff704d
soumyamaru123 <99595393+soumyamaru123@users.noreply.github.com>
2023-05-05 13:36:47 +0530

    Add files via upload

---
+++ compass.js
@@ -0,0 +1,30 @@
+Status = "";
+compass_image = "";
+
+function preload(){
+    compass_image = loadImage("compass.jpg");
+}
+
+function setup(){
+    canvas = createCanvas(640,350);
+    canvas.position(315,200);
+    object_Detector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    Status = true;
+    object_Detector.detect(compass_image,gotResults);
+}
+
+function gotResults(error,results){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+}
+
+function draw(){
+    image(compass_image,0,0,640,350);
+}
